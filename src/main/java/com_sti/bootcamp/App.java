0c8350e6191e4281c15048d56abfa7c510760103
muh.luqni <mluqnibaehaqi@gmail.com>
2018-12-11 08:52:49 +0700

    add put, delet and array list

--- src/main/java/com_sti/bootcamp/App.java
+++ src/main/java/com_sti/bootcamp/App.java
@@ -1,5 +1,9 @@
 package com_sti.bootcamp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,14 +37,13 @@ public class App
 //        return "Assalamu'alaikum " +data;
 //    }
 	@GetMapping("/custumer/{id}")
-	public Customer customer(@PathVariable ("id") String customerNumber) {
+	public Customer Createcustomer(@PathVariable ("id") String customerNumber) {
 		Customer cs = new Customer();
 		cs.setCustomerNumber(Integer.parseInt(customerNumber));
 		cs.setFirstName("luqni");
 		cs.setLastName("Baehaqi");
 		cs.setPhoneNumber("0874557732378");
 		cs.setPhoneType("Android");
-
 		return cs;
 	}
 	
@@ -49,15 +52,21 @@ public class App
 		return "hello" + customer.getFirstName();
 	}
 	@PutMapping("/customer")
-	public String Put(@RequestBody Customer customer) {
+	public String PuCustomert(@RequestBody Customer customer) {
+//		Customer cs = new Customer();
+//		cs.getCustomerNumber()
+//		return "hello" + customer.getFirstName();
+		return customer.getFirstName();
+	}
+	@DeleteMapping("/customer/{id}")
+	public String deleteCustomer(@PathVariable ("id") String customerNumber) {
 		Customer cs = new Customer();
+		cs.setCustomerNumber(Integer.parseInt(customerNumber));
+		return customerNumber;
 		
-		
-		return null;
-		
-//		return "Update Data :/t" + customer.getFirstName();
-
-		
+	}public List<Customer> getList(){
+		ArrayList<Customer> list = new ArrayList<Customer>();
+		return list;
 	}
 	
 	
